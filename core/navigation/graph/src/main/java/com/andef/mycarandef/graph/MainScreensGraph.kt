be7b32dc3ee25cac2c81b4e2a56b9e17af9528ff
dsemkin <semkin_dmitriy10@vk.com>
2025-07-09 23:46:18 +0400

    Навигация вроде тоже все (без экранов, но логика готова)

---
+++ core/navigation/graph/src/main/java/com/andef/mycarandef/graph/MainScreensGraph.kt
@@ -0,0 +1,33 @@
+package com.andef.mycarandef.graph
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.andef.mycarandef.di.viewmodel.ViewModelFactory
+import com.andef.mycarandef.routes.Screen
+
+fun NavGraphBuilder.mainScreensGraph(
+    navHostController: NavHostController,
+    viewModelFactory: ViewModelFactory,
+    paddingValues: PaddingValues
+) {
+    navigation(
+        route = Screen.MainScreens.route,
+        startDestination = Screen.MainScreens.WorksMainScreen.route
+    ) {
+        composable(route = Screen.MainScreens.WorksMainScreen.route) {
+
+        }
+        composable(route = Screen.MainScreens.ExpensesMainScreen.route) {
+
+        }
+        composable(route = Screen.MainScreens.MapsMainScreen.route) {
+
+        }
+        composable(route = Screen.MainScreens.CarsMainScreen.route) {
+
+        }
+    }
+}
