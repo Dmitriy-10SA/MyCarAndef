233c39cb342ab0e87337da7dc9eead690285640b
dsemkin <semkin_dmitriy10@vk.com>
2025-07-11 13:00:59 +0400

    Обернул кнопку все-таки)

--- app/src/main/java/com/andef/mycarandef/MainActivity.kt
+++ app/src/main/java/com/andef/mycarandef/MainActivity.kt
@@ -8,13 +8,11 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
