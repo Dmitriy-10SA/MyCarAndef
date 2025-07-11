49736f06d5bead554539d48d08df54a530db059b
dsemkin <semkin_dmitriy10@vk.com>
2025-07-11 22:56:50 +0400

    UiChooser сделал + добавил все иконки

--- app/src/main/java/com/andef/mycarandef/MainActivity.kt
+++ app/src/main/java/com/andef/mycarandef/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.andef.mycarandef
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -25,6 +27,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.andef.mycarandef.common.MyCarComponent
 import com.andef.mycarandef.design.button.ui.UiButton
+import com.andef.mycarandef.design.chooser.ui.UiChooser
 import com.andef.mycarandef.design.scaffold.ui.UiScaffold
 import com.andef.mycarandef.design.textfield.ui.UiTextField
 import com.andef.mycarandef.design.theme.DarkGray
@@ -102,16 +105,21 @@ private fun MainContent(
                     contentDescription = "",
                     leadingIcon = painterResource(com.andef.mycarandef.design.R.drawable.person)
                 )
-                UiTextField(
+                val context = LocalContext.current
+                UiChooser(
+                    onClick = {
+                        value2 = "image-12r4124123124-wefsdsf12=3=124124dsf"
+                    },
                     isLightTheme = isLightTheme,
                     value = value2,
-                    onValueChange = { value2 = it },
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp, vertical = 8.dp),
                     placeholderText = "Ваше имя",
-                    contentDescription = "",
-                    leadingIcon = painterResource(com.andef.mycarandef.design.R.drawable.person)
+                    leadingIconContentDescription = "",
+                    leadingIcon = painterResource(com.andef.mycarandef.design.R.drawable.image),
+                    trailingIcon = painterResource(com.andef.mycarandef.design.R.drawable.attach),
+                    trailingIconContentDescription = ""
                 )
                 UiButton(
                     text = "Продолжить",
