9edf21b4dbdd21260094b933825107d60e24ae78
dsemkin <semkin_dmitriy10@vk.com>
2025-07-11 13:18:08 +0400

    Scaffold сделал

--- core/design/src/main/java/com/andef/mycarandef/design/scaffold/ui/UiScaffold.kt
+++ core/design/src/main/java/com/andef/mycarandef/design/scaffold/ui/UiScaffold.kt
@@ -3,13 +3,10 @@ package com.andef.mycarandef.design.scaffold.ui
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.andef.mycarandef.design.theme.Black
 import com.andef.mycarandef.design.theme.DarkGray
 import com.andef.mycarandef.design.theme.White
