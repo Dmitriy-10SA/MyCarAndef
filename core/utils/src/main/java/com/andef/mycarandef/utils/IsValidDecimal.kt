78bb3bd548206830af978f9dbbf070007ace35d2
dsemkin <semkin_dmitriy10@vk.com>
2025-07-16 17:47:40 +0400

    Улучшил ExpenseMainScreen

--- core/utils/src/main/java/com/andef/mycarandef/utils/IsValidDecimal.kt
+++
@@ -1,6 +0,0 @@
-package com.andef.mycarandef.utils
-
-fun String.isValidDecimal(maxDecimals: Int = 2): Boolean {
-    val regex = Regex("^\\d{0,9}([.,]\\d{0,$maxDecimals})?$")
-    return matches(regex)
-}
