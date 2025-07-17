48cd4c1f592f46124b6b55574a48c8f8731b5036
dsemkin <semkin_dmitriy10@vk.com>
2025-07-18 00:20:48 +0400

    CarMainScreen готов

--- feature/car/presentation/src/main/java/com/andef/mycarandef/car/presentation/carmain/CarMainScreen.kt
+++ feature/car/presentation/src/main/java/com/andef/mycarandef/car/presentation/carmain/CarMainScreen.kt
@@ -1,28 +1,61 @@
 package com.andef.mycarandef.car.presentation.carmain
 
+import android.content.Context
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.andef.mycarandef.design.R
+import com.andef.mycarandef.design.alertdialog.ui.UiAlertDialog
+import com.andef.mycarandef.design.bottomsheet.ui.UiModalBottomSheet
 import com.andef.mycarandef.design.card.car.ui.UiCarCard
 import com.andef.mycarandef.design.error.ui.UiError
 import com.andef.mycarandef.design.loading.ui.UiLoading
+import com.andef.mycarandef.design.snackbar.type.UiSnackbarType
+import com.andef.mycarandef.design.snackbar.ui.UiSnackbar
+import com.andef.mycarandef.design.theme.Black
+import com.andef.mycarandef.design.theme.Blue
+import com.andef.mycarandef.design.theme.GrayForDark
+import com.andef.mycarandef.design.theme.GrayForLight
+import com.andef.mycarandef.design.theme.Red
+import com.andef.mycarandef.design.theme.White
+import com.andef.mycarandef.routes.Screen
 import com.andef.mycarandef.viewmodel.ViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarMainScreen(
     navHostController: NavHostController,
@@ -34,9 +67,250 @@ fun CarMainScreen(
     val state = viewModel.state.collectAsState()
 
     val context = LocalContext.current
+    val sheetState = rememberModalBottomSheetState()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) { viewModel.send(CarMainIntent.GetCars) }
 
+    MainContent(
+        paddingValues = paddingValues,
+        state = state,
+        viewModel = viewModel,
+        isLightTheme = isLightTheme,
+        context = context
+    )
+    BottomSheetWithDeleteDialog(
+        navHostController = navHostController,
+        viewModel = viewModel,
+        sheetState = sheetState,
+        isLightTheme = isLightTheme,
+        state = state,
+        scope = scope,
+        snackbarHostState = snackbarHostState
+    )
+    UiSnackbar(
+        paddingValues = paddingValues,
+        snackbarHostState = snackbarHostState,
+        type = UiSnackbarType.Error
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BottomSheetWithDeleteDialog(
+    navHostController: NavHostController,
+    viewModel: CarMainViewModel,
+    sheetState: SheetState,
+    isLightTheme: Boolean,
+    state: State<CarMainState>,
+    scope: CoroutineScope,
+    snackbarHostState: SnackbarHostState
+) {
+    state.value.carIdInBottomSheet?.let { carId ->
+        state.value.brandInBottomSheet?.let { brand ->
+            state.value.modelInBottomSheet?.let { model ->
+                UiModalBottomSheet(
+                    onDismissRequest = {
+                        viewModel.send(
+                            CarMainIntent.BottomSheetVisibleChange(isVisible = false)
+                        )
+                    },
+                    sheetState = sheetState,
+                    isLightTheme = isLightTheme,
+                    isVisible = state.value.showBottomSheet
+                ) {
+                    BottomSheetContent(
+                        isLightTheme = isLightTheme,
+                        brand = brand,
+                        model = model,
+                        onDeleteClick = {
+                            viewModel.send(
+                                CarMainIntent.ChangeDeleteDialogVisible(isVisible = true)
+                            )
+                        },
+                        year = state.value.yearInBottomSheet,
+                        registrationMark = state.value.registrationMarkOnBottomSheet,
+                        onChooseCurrentClick = {
+                            viewModel.send(
+                                CarMainIntent.BottomSheetVisibleChange(isVisible = false)
+                            )
+                            viewModel.send(
+                                CarMainIntent.ChooseCurrentCar(
+                                    carId = carId,
+                                    carName = "$brand $model",
+                                    carImageUri = state.value.imageUriInBottomSheet
+                                )
+                            )
+                        },
+                        onEditClick = {
+                            viewModel.send(
+                                CarMainIntent.BottomSheetVisibleChange(isVisible = false)
+                            )
+                            navHostController.navigate(
+                                Screen.CarScreen.passId(id = carId)
+                            )
+                        }
+                    )
+                }
+                DeleteDialog(
+                    isLightTheme = isLightTheme,
+                    carId = carId,
+                    viewModel = viewModel,
+                    scope = scope,
+                    state = state,
+                    snackbarHostState = snackbarHostState
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DeleteDialog(
+    carId: Long,
+    isLightTheme: Boolean,
+    viewModel: CarMainViewModel,
+    state: State<CarMainState>,
+    scope: CoroutineScope,
+    snackbarHostState: SnackbarHostState
+) {
+    UiAlertDialog(
+        isLightTheme = isLightTheme,
+        title = "Вы уверены?",
+        onDismissRequest = {
+            viewModel.send(
+                CarMainIntent.ChangeDeleteDialogVisible(isVisible = false)
+            )
+        },
+        onYesClick = {
+            viewModel.send(
+                CarMainIntent.ChangeDeleteDialogVisible(isVisible = false)
+            )
+            viewModel.send(
+                CarMainIntent.BottomSheetVisibleChange(isVisible = false)
+            )
+            viewModel.send(
+                CarMainIntent.DeleteCar(
+                    carId = carId,
+                    onError = { msg ->
+                        snackbarHostState.currentSnackbarData?.dismiss()
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                message = msg,
+                                withDismissAction = true
+                            )
+                        }
+                    }
+                )
+            )
+        },
+        onCancelClick = {
+            viewModel.send(
+                CarMainIntent.ChangeDeleteDialogVisible(isVisible = false)
+            )
+        },
+        isVisible = state.value.deleteDialogVisible
+    )
+}
+
+@Composable
+private fun BottomSheetContent(
+    isLightTheme: Boolean,
+    brand: String,
+    model: String,
+    registrationMark: String?,
+    year: Int?,
+    onDeleteClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onChooseCurrentClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+            .padding(bottom = 24.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Column {
+            Text(
+                text = "$brand $model",
+                fontSize = 16.sp,
+                color = if (isLightTheme) Black else White
+            )
+            if (registrationMark != null) {
+                val text = year?.let { "$registrationMark (${year}г.)" }
+                    ?: "$registrationMark (год не указан)"
+                Text(
+                    text = text,
+                    fontSize = 14.sp,
+                    color = if (isLightTheme) GrayForLight else GrayForDark
+                )
+            } else {
+                val text =
+                    year?.let { "Номер не указан (${year}г.)" } ?: "Номер не указан (год не указан)"
+                Text(
+                    text = text,
+                    fontSize = 14.sp,
+                    color = if (isLightTheme) GrayForLight else GrayForDark
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .clickable(onClick = onChooseCurrentClick)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.star),
+                tint = Blue,
+                contentDescription = "Звезда (сделать текущим)"
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = "Выбрать текущим",
+                color = Blue,
+                fontSize = 16.sp
+            )
+        }
+        Row(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .clickable(onClick = onEditClick)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.edit),
+                tint = if (isLightTheme) Black else White,
+                contentDescription = "Карандаш (изменить)"
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = "Изменить", color = if (isLightTheme) Black else White, fontSize = 16.sp)
+        }
+        Row(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .clickable(onClick = onDeleteClick)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.delete),
+                tint = Red,
+                contentDescription = "Корзина"
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = "Удалить", color = Red, fontSize = 16.sp)
+        }
+    }
+}
+
+@Composable
+private fun MainContent(
+    paddingValues: PaddingValues,
+    state: State<CarMainState>,
+    viewModel: CarMainViewModel,
+    isLightTheme: Boolean,
+    context: Context
+) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -48,8 +322,22 @@ fun CarMainScreen(
         item { Spacer(modifier = Modifier.height(0.dp)) }
         items(items = state.value.cars, key = { it.id }) { car ->
             UiCarCard(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = { TODO() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .animateItem(),
+                onClick = {
+                    viewModel.send(
+                        CarMainIntent.BottomSheetVisibleChange(
+                            isVisible = true,
+                            brand = car.brand,
+                            model = car.model,
+                            year = car.year,
+                            imageUriInBottomSheet = car.photo,
+                            registrationMark = car.registrationMark,
+                            carId = car.id
+                        )
+                    )
+                },
                 isLightTheme = isLightTheme,
                 car = car,
                 context = context
--- feature/car/presentation/src/main/java/com/andef/mycarandef/car/presentation/carmain/CarMainViewModel.kt
+++ feature/car/presentation/src/main/java/com/andef/mycarandef/car/presentation/carmain/CarMainViewModel.kt
@@ -3,16 +3,26 @@ package com.andef.mycarandef.car.presentation.carmain
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.andef.mycarandef.car.domain.usecases.GetAllCarsUseCase
+import com.andef.mycarandef.car.domain.usecases.RemoveCarUseCase
+import com.andef.mycarandef.car.domain.usecases.SetCurrentCarIdUseCase
+import com.andef.mycarandef.car.domain.usecases.SetCurrentCarImageUriUseCase
+import com.andef.mycarandef.car.domain.usecases.SetCurrentCarNameUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CarMainViewModel @Inject constructor(
-    private val getAllCarsUseCase: GetAllCarsUseCase
+    private val getAllCarsUseCase: GetAllCarsUseCase,
+    private val removeCarUseCase: RemoveCarUseCase,
+    private val setCurrentCarIdUseCase: SetCurrentCarIdUseCase,
+    private val setCurrentCarNameUseCase: SetCurrentCarNameUseCase,
+    private val setCurrentCarImageUriUseCase: SetCurrentCarImageUriUseCase
 ) : ViewModel() {
     private val _state = MutableStateFlow(CarMainState())
     val state: StateFlow<CarMainState> = _state
@@ -20,7 +30,74 @@ class CarMainViewModel @Inject constructor(
     fun send(intent: CarMainIntent) {
         when (intent) {
             CarMainIntent.GetCars -> getCars()
+            is CarMainIntent.BottomSheetVisibleChange -> changeBottomSheetVisible(
+                isVisible = intent.isVisible,
+                registrationMark = intent.registrationMark,
+                imageUriInBottomSheet = intent.imageUriInBottomSheet,
+                year = intent.year,
+                model = intent.model,
+                brand = intent.brand,
+                carId = intent.carId
+            )
+
+            is CarMainIntent.ChangeDeleteDialogVisible -> changeDeleteDialogVisible(
+                isVisible = intent.isVisible
+            )
+
+            is CarMainIntent.DeleteCar -> deleteCar(
+                carId = intent.carId,
+                onError = intent.onError
+            )
+
+            is CarMainIntent.ChooseCurrentCar -> chooseCurrentCar(
+                carId = intent.carId,
+                carName = intent.carName,
+                carImageUri = intent.carImageUri
+            )
         }
+    }
+
+    private fun chooseCurrentCar(carId: Long, carName: String, carImageUri: String?) {
+        setCurrentCarIdUseCase.invoke(carId)
+        setCurrentCarNameUseCase.invoke(carName)
+        setCurrentCarImageUriUseCase.invoke(carImageUri)
+    }
+
+    private fun changeDeleteDialogVisible(isVisible: Boolean) {
+        _state.value = _state.value.copy(deleteDialogVisible = isVisible)
+    }
+
+    private fun deleteCar(carId: Long, onError: (String) -> Unit) {
+        viewModelScope.launch {
+            try {
+                _state.value = _state.value.copy(isLoading = true)
+                withContext(Dispatchers.IO) { removeCarUseCase.invoke(carId) }
+            } catch (_: Exception) {
+                onError("Ошибка! Попробуйте ещё раз!")
+            } finally {
+                _state.value = _state.value.copy(isLoading = false)
+            }
+        }
+    }
+
+    private fun changeBottomSheetVisible(
+        isVisible: Boolean,
+        registrationMark: String? = null,
+        year: Int? = null,
+        model: String? = null,
+        imageUriInBottomSheet: String? = null,
+        brand: String? = null,
+        carId: Long? = null
+    ) {
+        _state.value = _state.value.copy(
+            showBottomSheet = isVisible,
+            registrationMarkOnBottomSheet = registrationMark,
+            brandInBottomSheet = brand,
+            imageUriInBottomSheet = imageUriInBottomSheet,
+            modelInBottomSheet = model,
+            yearInBottomSheet = year,
+            carIdInBottomSheet = carId
+        )
     }
 
     private var job: Job? = null
