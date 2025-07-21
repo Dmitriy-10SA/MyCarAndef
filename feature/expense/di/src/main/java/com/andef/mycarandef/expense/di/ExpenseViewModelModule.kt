809828bd0c8893732cc532a1e7605ca14eeb5450
dsemkin <semkin_dmitriy10@vk.com>
2025-07-21 12:34:13 +0400

    di провел для анализа трат (ViewModel)

--- feature/expense/di/src/main/java/com/andef/mycarandef/expense/di/ExpenseViewModelModule.kt
+++ feature/expense/di/src/main/java/com/andef/mycarandef/expense/di/ExpenseViewModelModule.kt
@@ -2,6 +2,7 @@ package com.andef.mycarandef.expense.di
 
 import androidx.lifecycle.ViewModel
 import com.andef.mycarandef.expense.presentation.expenseadd.ExpenseAddViewModel
+import com.andef.mycarandef.expense.presentation.expenseanalysis.ExpenseAnalysisViewModel
 import com.andef.mycarandef.expense.presentation.expensemain.ExpenseMainViewModel
 import com.andef.mycarandef.viewmodel.ViewModelKey
 import dagger.Binds
@@ -19,4 +20,9 @@ interface ExpenseViewModelModule {
     @IntoMap
     @ViewModelKey(ExpenseAddViewModel::class)
     fun bindExpenseAddViewModel(impl: ExpenseAddViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ExpenseAnalysisViewModel::class)
+    fun bindExpenseAnalysisViewModel(impl: ExpenseAnalysisViewModel): ViewModel
 }
