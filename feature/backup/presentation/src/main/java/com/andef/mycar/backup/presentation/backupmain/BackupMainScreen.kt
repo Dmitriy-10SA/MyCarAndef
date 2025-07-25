b0f1f8157931d3b42bd0425f8f73ef562c740bdb
dsemkin <semkin_dmitriy10@vk.com>
2025-07-25 15:20:37 +0400

    Вынес Gson для backup в общий файл

--- feature/backup/presentation/src/main/java/com/andef/mycar/backup/presentation/backupmain/BackupMainScreen.kt
+++ feature/backup/presentation/src/main/java/com/andef/mycar/backup/presentation/backupmain/BackupMainScreen.kt
@@ -50,20 +50,10 @@ import com.andef.mycarandef.design.theme.White
 import com.andef.mycarandef.design.topbar.type.UiTopBarType
 import com.andef.mycarandef.design.topbar.ui.UiTopBar
 import com.andef.mycarandef.viewmodel.ViewModelFactory
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonDeserializationContext
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonElement
-import com.google.gson.JsonPrimitive
-import com.google.gson.JsonSerializationContext
-import com.google.gson.JsonSerializer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.io.File
-import java.lang.reflect.Type
 import java.time.LocalDate
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 @Composable
 fun BackupMainScreen(
